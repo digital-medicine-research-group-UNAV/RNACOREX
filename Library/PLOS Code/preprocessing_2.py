c7ba35e305256c5fd7c7751006f57c761c97cbee
aitoroviedo6 <144782530+aitoroviedo6@users.noreply.github.com>
2025-03-24 11:17:16 +0100

    Delete Library/PLOS Code directory

--- Library/PLOS Code/preprocessing_2.py
+++
@@ -1,8 +0,0 @@
-from data_preprocessing import preprocessing_2
-from tqdm import tqdm
-
-notebook_names = ['brca', 'coad', 'hnsc', 'kirc', 'laml', 'lgg', 'lihc', 'luad', 'lusc', 'ov', 'sarc', 'skcm', 'stad', 'ucec']
-
-for notebook in tqdm(notebook_names):
-
-    preprocessing_2(notebook)
