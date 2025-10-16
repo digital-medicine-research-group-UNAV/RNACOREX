a58914a9a04120b449b9a23e1266874d94f9b40a
aitoroviedo6 <144782530+aitoroviedo6@users.noreply.github.com>
2025-10-16 15:35:46 +0200

    Delete Library/RNACOREX 0.1 directory

--- Library/RNACOREX 0.1/rnacorex/PLOS code/execute_preprocessing.py
+++
@@ -1,7 +0,0 @@
-from preprocessing import preprocessing_rnacorex
-
-bbdds = ['brca', 'coad', 'hnsc', 'kirc', 'laml', 'lgg', 'lihc', 'luad', 'lusc', 'sarc', 'skcm', 'stad', 'ucec']
-
-for bbdd in bbdds:
-    print(f'Processing {bbdd}...')
-    preprocessing_rnacorex(bbdd)
--- Library/RNACOREX 0.1/rnacorex/__init__.py
+++
@@ -1,6 +0,0 @@
-from .miRNetClassifier import MRNC
-from .utils import check_engines, download
-
-__all__ = [
-    "MRNC"
-]
